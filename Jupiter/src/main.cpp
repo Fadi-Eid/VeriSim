148017d9b96522e1d9278c4bd25dfbfbda869b80
Fadi EID <fadieid.eng@gmail.com>
2025-09-06 13:51:38 +0000

    Added Raylib dependency to CMAKE file

--- Jupiter/src/main.cpp
+++ Jupiter/src/main.cpp
@@ -4,8 +4,11 @@
 #include "fileInput.hpp"
 #include "fileOutput.hpp"
 #include "logHelper.hpp"
+#include "raylib.h"
 
 using namespace ns_jupiter;
+
+
 
 int main(int argc, char* argv[]) {
     // Instantiate module
@@ -28,6 +31,17 @@ int main(int argc, char* argv[]) {
         fileOut.append(pair.time_stamp_ns, pair.value, OutputFormat::HEX);
     }
 
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
   
     return 0;
 }
